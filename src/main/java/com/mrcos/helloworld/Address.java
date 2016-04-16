0df9b4b9aa59d27e73ebcb932abfb5ea0a7c511e
Madhu Reddy <Madhu@MR-VAIO.(none)>
2016-04-15 23:30:20 -0400

    Added Bean Scopes

--- src/main/java/com/mrcos/helloworld/Address.java
+++ src/main/java/com/mrcos/helloworld/Address.java
@@ -4,6 +4,10 @@ public class Address {
 	private String street;
 	private String postcode;
 
+	public void setPostcode(String postcode) {
+		this.postcode = postcode;
+	}
+
 	@Override
 	public String toString() {
 		return "Address [street=" + street + ", postcode=" + postcode + "]";
